4ec16b35d496d1c1598258fd84aed0090de744dd
whuera <whuera@gmail.com>
2021-11-07 10:12:09 -0500

    update settings

---
+++ src/test/java/com/mobilpymes/web/starter/StarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobilpymes.web.starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
